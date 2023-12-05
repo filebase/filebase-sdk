04d2b6cb52118cc39f05373a312ec711c5960098
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2023-12-05 16:29:27 -0600

    Update bucketManager.js
    Add examples

--- src/bucketManager.js
+++ src/bucketManager.js
@@ -12,6 +12,9 @@ class BucketManager {
   /**
    * @summary Creates a new instance of the S3Client class.
    * @param {Object} clientConfig - The configuration object for the S3Client.
+   * @example
+   * import { bucketManager } from "@filebase/sdk";
+   * const bucketManager = new BucketManager(S3_CONFIG);
    */
   constructor(clientConfig) {
     this.#client = new S3Client(clientConfig);
@@ -21,6 +24,9 @@ class BucketManager {
    * @summary Creates a new bucket with the specified name.
    * @param {string} name - The name of the bucket to create.
    * @returns {Promise<*>} - A promise that resolves when the bucket is created.
+   * @example
+   * // Create bucket with name of `create-bucket-example`
+   * await bucketManager.create(`create-bucket-example`);
    */
   async create(name) {
     const command = new CreateBucketCommand({
@@ -34,6 +40,9 @@ class BucketManager {
    * @summary Lists the buckets in the client.
    * @param {Object} listBucketOptions - The options for listing buckets.
    * @returns {Promise<Array<Object>>} - A promise that resolves with an array of objects representing the buckets in the client.
+   * @example
+   * // List all buckets
+   * await bucketManager.list();
    */
   async list(listBucketOptions = {}) {
     const command = new ListBucketsCommand(listBucketOptions),
@@ -46,6 +55,9 @@ class BucketManager {
    * @summary Deletes the specified bucket.
    * @param {string} name - The name of the bucket to delete.
    * @returns {Promise<void>} - A promise that resolves when the bucket is deleted.
+   * @example
+   * // Delete bucket with name of `bucket-name-to-delete`
+   * await bucketManager.delete(`bucket-name-to-delete`);
    */
   async delete(name) {
     const command = new DeleteBucketCommand({
